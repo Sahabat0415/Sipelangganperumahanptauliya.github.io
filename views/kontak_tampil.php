238077812049e46634421336325d47e33c6737d8
Imam0415 <60427493+Imam0415@users.noreply.github.com>
2020-01-30 19:20:53 +0700

    Add files via upload

---
+++ views/kontak_tampil.php
@@ -0,0 +1,74 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PERUMAHAN <br> PT. AULIA KISARAN</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                                   
+            <!--Jika terjadi login error tampilkan pesan ini-->
+            <?php if(isset($_GET['error']) ) {?>
+            <div class="alert alert-danger">Maaf! Login Gagal, Coba Lagi..</div>
+            <?php }?>
+
+            <?php if (isset($_SESSION['username'])) { ?>
+            <div class="alert alert-info">
+                <strong>Welcome <?=$_SESSION['nama']?></strong>
+            </div>
+            <?php
+           } else { ?>
+
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Masuk Ke Sistem</h3>
+                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" action="proses_login.php" method="post">
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <input type="text" name="user" class="form-control input-sm"
+                                   placeholder="Username" required="" autocomplete="off"/>
+                            </div>
+
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <input type="password" name="pwd" class="form-control input-sm"
+                                   placeholder="Password" required="" autocomplete="off"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <button type="submit" name="login" value="login"
+                                        class="btn btn-info btn-block"><span class="fa fa-unlock-alt"></span>
+                                    Login Sistem
+                                </button>
+                            </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+            <?php } ?>
+ 
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
